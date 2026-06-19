996ef636b60b89a5b349bebe568e64961b126f5a
NandaKumarMatha <nandakumarmatha98@gmail.com>
2026-06-19 17:19:31 +0530

    chore: update dependencies in setup.py and snapcraft.yaml, bump version to 0.2.44, and add launcher script for improved execution

--- setup.py
+++ setup.py
@@ -7,8 +7,11 @@ setup(
     py_modules=["main"],
     install_requires=[
         "PyQt6>=6.0.0",
+        "PyQt6-WebEngine>=6.0.0",
         "pyarrow>=10.0.0",
         "pandas>=1.5.0",
+        "qdarkstyle",
+        "plotly",
     ],
     entry_points={
         "console_scripts": [
